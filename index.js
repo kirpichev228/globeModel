2de4c5c05d236499946aa33c86608f99f6ecb8e3
kirpichev228 <kirpichev228@mail.ru>
2023-08-26 20:00:03 +0300

    readme update

--- index.js
+++ index.js
@@ -82,18 +82,3 @@ export function globeDraw(options = {}) {
 }
 
 globeDraw();
-
-// можно передать как аргумент объект опций с необходимыми значениями, не обязательно указывать все. Значения по умолчанию:
-// {
-//     sceneWidth = window.innerWidth,                                  ширина сцены
-//     sceneHeight = window.innerHeight,                                высота сцены
-//     backgroundColor = '#2f3035',                                     цвет задника, можно словом типа red
-//     modelColor = 0xFFFFFF,                                           цвет модельки, обязательно шестнадцатеричным и не строкой
-//     objectPosition = { x: 0, y: 0, z: 0 },                           позиция объекта в пространстве
-//     objectRotation = { x: Math.PI * 1.3, y: Math.PI / 1.8, z: 0 },   изначальное положение относительно собственных осей объекта
-//     mouseDrag = false,                                               можно ли тягать глобус мышкой
-//     dragSpeed = 0.001,                                               скорость вращения
-//     autoSpeed = 0,                                                   скорость автоматического вращения
-//     zoom = 180,                                                      зум
-//     pathToObj = '/globus/Severin earth model (1) (1).obj'            откуда взять объект
-// }
